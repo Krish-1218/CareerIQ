f38296e5603f1ce43990484f6264b36588e35c4d
Krish-1218 <ksbhise1812@gmail.com>
2025-06-26 22:42:10 +0530

    Update layout.js
     [footer] text-center and text-gray

--- app/layout.js
+++ app/layout.js
@@ -33,8 +33,8 @@ export default function RootLayout({ children }) {
             {/*header*/}
             <main className="min-h-screen">{children}</main>
             {/* footer */}
-            <footer>
-              <div>
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>G6 Working on it</p>
               </div>
             </footer>
