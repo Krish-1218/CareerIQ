549708775081122225e8fd37c0b45e4eccd1506d
Krish-1218 <ksbhise1812@gmail.com>
2025-06-26 22:55:53 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +31,7 @@ export default function RootLayout({ children }) {
             disableTransitionOnChange
           >
             {/*header*/}
+            <Header/>
             <main className="min-h-screen">{children}</main>
             {/* footer */}
             <footer className="bg-muted/50 py-12">
