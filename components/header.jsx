966ef4acc13f52b61c04eee455427938658bcc22
Krish-1218 <ksbhise1812@gmail.com>
2025-06-27 19:10:03 +0530

    Update header.jsx
    Added CareerIQ Logo – represents our AI-powered smart career guidance platform.

--- components/header.jsx
+++ components/header.jsx
@@ -1,15 +1,27 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import React from "react";
+import Link from "next/link";
+import Image from "next/image"; 
 
 const Header = () => {
-    return  <div>
+    return  <header>
+        <nav>
+            <Link href="/">
+              <Image
+              src ="/logo.png" 
+              alt="CareerIQ Logo" 
+              width={200} 
+              height={60} 
+              className= "h-20 py-1 w-auto object-fill" />
+            </Link>
+        </nav>
+
         <SignedOut>
             <SignInButton/>    
             </SignedOut>
         <SignedIn>
             <UserButton/>
         </SignedIn>
-    </div>;
+    </header>;
 };
 
 export default Header;
