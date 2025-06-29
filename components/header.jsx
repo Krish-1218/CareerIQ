5be2abe8cd18e5b77186fae74d56a659fa40530d
Krish-1218 <ksbhise1812@gmail.com>
2025-06-29 22:05:11 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,41 +1,90 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import Image from "next/image"; 
+import Image from "next/image";
 import { Button } from "./ui/button";
-import { LayoutDashboard } from "lucide-react";
+import {
+  ChevronDown,
+  FileText,
+  GraduationCap,
+  LayoutDashboard,
+  PenBox,
+  StarsIcon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
-    return  <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+  return (
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="CareerIQ Logo"
+            width={300}
+            height={100}
+            className="h-11 py-1 w-auto object-fill"
+          />
+        </Link>
 
-        <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
-            <Link href="/">
-              <Image
-              src ="/logo.png" 
-              alt="CareerIQ Logo" 
-              width={300} 
-              height={100} 
-              className= "h-11 py-1 w-auto object-fill" />
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <SignedIn>
+            <Link href={"/dashboard"}>
+              <Button>
+                <LayoutDashboard className="h-4 w-4" />
+                <span className="hidden md:block">Industry Insights </span>
+              </Button>
             </Link>
+          </SignedIn>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button>
+                <StarsIcon className="h-4 w-4" />
+                <span className="hidden md:block">Growth Tools</span>
+                <ChevronDown className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem asChild>
+                <Link href="/resume" className="flex items-center gap-2">
+                  <FileText className="h-4 w-4" />
+                  Build Resume
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link
+                  href="/ai-cover-letter"
+                  className="flex items-center gap-2">
+                  <PenBox className="h-4 w-4" />
+                  Cover Letter
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/interview" className="flex items-center gap-2">
+                  <GraduationCap className="h-4 w-4" />
+                  Interview Prep
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          ̥̥
+        </div>
+      </nav>
 
-            <div>
-                <SignedIn>
-                    <Link href={'/dashboard'}>
-                    <Button>
-                        <LayoutDashboard className="h-4 w-4"/>
-                        Industry Insights
-                    </Button>
-                    </Link>
-                </SignedIn>
-            </div>
-        </nav>
-
-        <SignedOut>
-            <SignInButton/>    
-            </SignedOut>
-        <SignedIn>
-            <UserButton/>
-        </SignedIn>
-    </header>;
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 };
 
 export default Header;
